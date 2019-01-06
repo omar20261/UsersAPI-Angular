9ccdf4b9537928e77c4f0b02c57b7f19b0ac832d
mero mero <mero20261@gmail.com>
2019-01-06 17:22:59 +0200

    fix overlap tokens error

--- src/app/Services/Auth/auth.service.ts
+++ src/app/Services/Auth/auth.service.ts
@@ -8,16 +8,45 @@ export class AuthService {
 
   constructor(private router:Router,public jwtHelper: JwtHelperService) { }
 
-  loggedIn(){ return !this.jwtHelper.isTokenExpired(this.loadToken()); }
+  /**
+    * @purpose = check user status
+    * @param = none
+    * @return = boolean value
+  */
+  loggedIn(){
+    try { return !this.jwtHelper.isTokenExpired(this.loadToken()); }
+    catch(e) { localStorage.removeItem('token'); return false }
+  }
 
-  logout(){ localStorage.removeItem('token');
-      this.router.navigate(['/login']);}
+  /**
+    * @purpose = logout
+    * @param = none
+    * @return = none
+  */
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 
-  storeToken(token){
-      localStorage.setItem("token",token);}
+  /**
+    * @purpose = store Token
+    * @param = none
+    * @return = none
+  */
+  storeToken(token){ localStorage.setItem("token",token);}
 
+  /**
+    * @purpose = Token getter
+    * @param = none
+    * @return = token
+  */
   loadToken(){return localStorage.getItem("token"); }
 
+  /**
+    * @purpose = load User info
+    * @param = none
+    * @return = User info
+  */
   loadUser(){
     let token = this.loadToken();
     return token?this.jwtHelper.decodeToken(token):{};
